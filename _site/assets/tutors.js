9304330ba9cc3a4638b3bdb2b5c1a427cd621b93
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 18:10:10 -0400

    v1.1.3
    upload tutors

---
+++ _site/assets/tutors.js
@@ -0,0 +1,15 @@
+
+
+
+
+$.getJSON('http://gsx2json.com/api?id=15UJ5-Tg_3D3Eeg1VfY9XFiBphAVmQoBgxDKePPrSzHQ', (data)=>{
+    (data.rows).forEach(t => {
+        var c = $('<div>').addClass('tutor').appendTo('#tutor-con');
+        if (t.image != ''){
+            $('<img>').attr('src',t.image).appendTo(c);
+        } else {$('<img>').attr('src','https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg').appendTo(c);}
+        $('<h2>').text(t.name).appendTo(c);
+        $('<p>').text(t.school).appendTo(c);
+
+    });
+})
