860469aec62238bacbb4ffce09bd9e6c13616cfc
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 19:12:54 -0400

    hi

--- _site/assets/tutors.js
+++ _site/assets/tutors.js
@@ -3,13 +3,13 @@
 
 
 $.getJSON('https://sheet.best/api/sheets/c82f0581-abd9-4b43-ba80-f0237a5656a5', (data)=>{
-    (data.rows).forEach(t => {
+    (data).forEach(t => {
         var c = $('<div>').addClass('tutor').appendTo('#tutor-con');
-        if (t.image != ''){
-            $('<img>').attr('src',t.image).appendTo(c);
+        if (t.Image != null){
+            $('<img>').attr('src',t.Image).appendTo(c);
         } else {$('<img>').attr('src','https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg').appendTo(c);}
-        $('<h2>').text(t.name).appendTo(c);
-        $('<p>').text(t.school).appendTo(c);
+        $('<h2>').text(t.Name).appendTo(c);
+        $('<p>').text(t.School).appendTo(c);
 
     });
 })
