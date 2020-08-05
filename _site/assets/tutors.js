93a13f3d6d4287de368d1ff85b3a516216fc037c
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 18:29:02 -0400

    v1.1.3

--- _site/assets/tutors.js
+++ _site/assets/tutors.js
@@ -2,7 +2,7 @@
 
 
 
-$.getJSON('http://gsx2json.com/api?id=15UJ5-Tg_3D3Eeg1VfY9XFiBphAVmQoBgxDKePPrSzHQ', (data)=>{
+$.getJSON('https://gsx2json.com/api?id=15UJ5-Tg_3D3Eeg1VfY9XFiBphAVmQoBgxDKePPrSzHQ', (data)=>{
     (data.rows).forEach(t => {
         var c = $('<div>').addClass('tutor').appendTo('#tutor-con');
         if (t.image != ''){
