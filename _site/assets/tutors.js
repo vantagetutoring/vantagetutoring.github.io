66f7fcfe2976f86e63b1b14be5e9611ec397dd9b
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 19:10:52 -0400

    v1.1.4

--- _site/assets/tutors.js
+++ _site/assets/tutors.js
@@ -2,7 +2,7 @@
 
 
 
-$.getJSON('http://gsx2json.com/api?id=15UJ5-Tg_3D3Eeg1VfY9XFiBphAVmQoBgxDKePPrSzHQ', (data)=>{
+$.getJSON('https://sheet.best/api/sheets/c82f0581-abd9-4b43-ba80-f0237a5656a5', (data)=>{
     (data.rows).forEach(t => {
         var c = $('<div>').addClass('tutor').appendTo('#tutor-con');
         if (t.image != ''){
