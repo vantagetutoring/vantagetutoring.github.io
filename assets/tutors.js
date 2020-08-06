d8a5202b7f11c8d094b2b9fe0b67a08398f3d539
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 20:11:09 -0400

    v1.1.5
    photo to g sheets

--- assets/tutors.js
+++ assets/tutors.js
@@ -4,12 +4,32 @@
 
 $.getJSON('https://sheet.best/api/sheets/c82f0581-abd9-4b43-ba80-f0237a5656a5', (data)=>{
     (data).forEach(t => {
-        var c = $('<div>').addClass('tutor').appendTo('#tutor-con');
-        if (t.Image != null){
+        var con = $('<div>').addClass('tutor').appendTo('#tutor-con');
+        var c = $('<div>').attr('id','first').appendTo(con);
+        if (t.Image != ""){
             $('<img>').attr('src',t.Image).appendTo(c);
         } else {$('<img>').attr('src','https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg').appendTo(c);}
         $('<h2>').text(t.Name).appendTo(c);
         $('<p>').text(t.School).appendTo(c);
 
+        
+
+        
+
+        con.click(()=>{
+            var d = $('<div>').attr('id','second').appendTo('main');
+            var e = $('<div>').attr('id','second-con').appendTo(d);
+            var e1 = $('<div>').appendTo(e);
+            $('<img>').appendTo(e1).attr('src',t.Image);
+            $('<h2>').text(t.Name).appendTo(e1);
+            var e2 = $('<div>').attr('id','second-con-2').appendTo(e);
+            $('<p>').text(t.Bio).appendTo(e2);
+            d.click(e=>{
+                if (e.target.id == 'second'){
+                    d.remove();
+                }
+            })
+        });
+        
     });
 })
