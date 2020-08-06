ee5567e7d78c79b29e3bcf01dfd322d83ade6489
Vantage Tutoring <68923189+vantagetutoring@users.noreply.github.com>
2020-08-05 21:36:09 -0400

    v1.1.05

--- assets/tutors.js
+++ assets/tutors.js
@@ -12,6 +12,7 @@ $.getJSON('https://sheet.best/api/sheets/c82f0581-abd9-4b43-ba80-f0237a5656a5', 
         $('<h2>').text(t.Name).appendTo(c);
         $('<p>').text(t.School).appendTo(c);
 
+
         
 
         
@@ -19,11 +20,17 @@ $.getJSON('https://sheet.best/api/sheets/c82f0581-abd9-4b43-ba80-f0237a5656a5', 
         con.click(()=>{
             var d = $('<div>').attr('id','second').appendTo('main');
             var e = $('<div>').attr('id','second-con').appendTo(d);
-            var e1 = $('<div>').appendTo(e);
+            var e1 = $('<div>').appendTo(e).attr('id','second-con-1');
             $('<img>').appendTo(e1).attr('src',t.Image);
             $('<h2>').text(t.Name).appendTo(e1);
+            $('<button>').appendTo(e1).text('Close').click(()=>{
+                d.remove();
+            });
             var e2 = $('<div>').attr('id','second-con-2').appendTo(e);
-            $('<p>').text(t.Bio).appendTo(e2);
+            $('<p>').html('<b>Grade </b> - ' + t.Grade).appendTo(e2);
+            $('<p>').html('<b>School </b> - ' + t.School).appendTo(e2);
+            $('<p>').html('<b>Teaches </b> - ' + t.Subjects).appendTo(e2);
+            $('<p>').html(t.Bio).appendTo(e2);
             d.click(e=>{
                 if (e.target.id == 'second'){
                     d.remove();
